46669d7fb4eac7537a449e885a784b19869ec51e
Raquel <oemaildaraquel@gmail.com>
2018-07-04 09:39:24 -0300

    testes repetidos deletados

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -13,21 +13,7 @@ public class SessaoTest {
         Assert.assertTrue(sessao.podeReservar(2));
 	}
 	
-	@Test
-	public void deveVender5ingressosSeHa10vagas() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(10);
-		
-		Assert.assertTrue(sessao.podeReservar(5));
-	}
-
-	@Test
-	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(2);
-
-		Assert.assertFalse(sessao.podeReservar(3));
-	}
+	
 
 	@Test
 	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
